cde9d5b58165f8bea5d0826153d98fa5d9016fd5
Khushi Sanghvi <khushisanghvi940@gmail.com>
2023-04-21 18:26:16 +0530

    Seccond Commit

--- src/assets/index.js
+++ src/assets/index.js
@@ -22,7 +22,7 @@ import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
-import myPic from './myPic.jpg'
+import myPic from './mypic.jpg'
 import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
